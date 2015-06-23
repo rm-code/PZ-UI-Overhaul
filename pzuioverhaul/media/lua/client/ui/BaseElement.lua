40d8ea79b4c268d4c5e71a4317d20220c054f43c
Benjamin 'blindCoder' Schieder <benjamin@anderdonau.de>
2015-06-23 14:46:19 +0200

    rearrange and rename

--- pzuioverhaul/media/lua/client/ui/BaseElement.lua
+++ pzuioverhaul/media/lua/client/ui/BaseElement.lua
@@ -1,42 +1,28 @@
 local MAX_X = getCore():getScreenWidth();
 local MAX_Y = getCore():getScreenHeight();
 
-BaseElement = {};
+UIO = {};
+UIO.BaseElement = {};
 
-function BaseElement.new(x, y, w, h)
+function UIO.BaseElement.new(x, y, w, h)
     local self = {};
-
-    -- Clamp the position values so the UI can't be moved offscreen.
-    self:setPosition(x, y);
-
-    -- Create a java instance.
-    local jObj = UIElement.new(self);
-    jObj:setX(x);
-    jObj:setY(y);
-    jObj:setWidth(w);
-    jObj:setHeight(h);
-    jObj:setAnchorLeft(false);
-    jObj:setAnchorRight(false);
-    jObj:setAnchorTop(false);
-    jObj:setAnchorBottom(false);
+		local jObj = {};
 
     -- ------------------------------------------------
     -- Local Functions
     -- ------------------------------------------------
-
-    local function clamp(min, val, max)
+    local function clamp(min, val, max) -- {{{ clamp value to valid range
         return math.max(min, math.min(val, max));
     end
-
+		-- }}}
     -- ------------------------------------------------
     -- Public Functions
     -- ------------------------------------------------
-
-    function self:addToUIManager()
+    function self:addToUIManager() -- {{{ add element to UI
         UIManager.AddUI(jObj);
     end
-
-    function self:drawRectangle(mode, x, y, w, h, color)
+		-- }}}
+    function self:drawRectangle(mode, x, y, w, h, color) -- {{{ draw a rectangle, filled or line
         if mode == 'fill' then
             jObj:DrawTextureScaledColor(nil, x, y, w, h, color.r, color.g, color.b, color.a);
         elseif mode == 'line' then
@@ -46,58 +32,69 @@ function BaseElement.new(x, y, w, h)
             jObj:DrawTextureScaledColor(nil, x, y + h, w, 1, color.r, color.g, color.b, color.a);
         end
     end
-
-    function self:close()
+		-- }}}
+    function self:close() -- {{{ remove window from UI Manager
         self:setVisible(false);
         UIManager.RemoveElement(jObj);
     end
-
-    function self:toggle()
+		-- }}}
+    function self:toggle() -- {{{ toggle visibility
         self:setVisible(not jObj:isVisible());
     end
-
+		-- }}}
     -- ------------------------------------------------
     -- Setters
     -- ------------------------------------------------
-
-    function self:setVisible(nv)
+    function self:setVisible(nv) -- {{{ set visibility
         jObj:setVisible(nv);
     end
-
+		-- }}}
+    function self:setX(nx) -- {{{
+        x = clamp(0, nx + w, MAX_X);
+    end
+		-- }}}
+    function self:setY(ny) -- {{{
+        y = clamp(0, ny + h, MAX_Y);
+    end
+		-- }}}
+    function self:setPosition(nx, ny) -- {{{
+        x = clamp(0, nx + w, MAX_X);
+        y = clamp(0, ny + h, MAX_Y);
+    end
+		-- }}}
     -- ------------------------------------------------
     -- Getters
     -- ------------------------------------------------
-
-    function self:isVisible()
+    function self:isVisible() -- {{{
         return jObj:isVisible();
     end
-
-    function self:getX()
+		-- }}}
+    function self:getX() -- {{{
         return x;
     end
-
-    function self:getY()
+		-- }}}
+    function self:getY() -- {{{
         return y;
     end
-
-    function self:getPosition()
+		-- }}}
+    function self:getPosition() -- {{{
         return x, y;
     end
+		-- }}}
 
-    function self:setX(nx)
-        x = clamp(0, nx + w, MAX_X);
-    end
+    -- Create a java instance.
+    jObj = UIElement.new(self);
+    -- Clamp the position values so the UI can't be moved offscreen.
+    self:setPosition(x, y);
 
-    function self:setY(ny)
-        y = clamp(0, ny + h, MAX_Y);
-    end
-
-    function self:setPosition(nx, ny)
-        x = clamp(0, nx + w, MAX_X);
-        y = clamp(0, ny + h, MAX_Y);
-    end
+    jObj:setX(x);
+    jObj:setY(y);
+    jObj:setWidth(w);
+    jObj:setHeight(h);
+    jObj:setAnchorLeft(false);
+    jObj:setAnchorRight(false);
+    jObj:setAnchorTop(false);
+    jObj:setAnchorBottom(false);
 
     return self;
 end
-
-return BaseElement;
