7216d3cb598a6083fcc688cf1413d8ce2cf51192
Benjamin 'blindCoder' Schieder <benjamin@anderdonau.de>
2015-06-23 16:27:20 +0200

    implement a panel and a draggable option

---
+++ pzuioverhaul/media/lua/client/uio/Draggable.lua
@@ -0,0 +1,59 @@
+require 'uio/BaseElement'
+
+UIO.Draggable = {};
+
+function UIO.Draggable.new(x, y, w, h)
+	local isDragging = false;
+	local dragStart = { x = 0, y = 0 };
+	local self = UIO.BaseElement.new(x, y, w, h);
+
+	function self:onMouseDown(mX, mY) -- {{{
+		local parent = self:getParent();
+		if not parent then return false end
+		isDragging = true;
+		dragStart = { x = getMouseX(), y = getMouseY() };
+		return true;
+	end
+	-- }}}
+	function self:onMouseUp(mX, mY) -- {{{
+		isDragging = false;
+		return true;
+	end
+	-- }}}
+	function self:onMouseMove(mX, mY) -- {{{
+		local parent = self:getParent();
+		if not parent then return false end
+		mX = getMouseX();
+		mY = getMouseY();
+		if not isDragging then return parent:onMouseMove(mX, mY) end
+
+		parent:setPosition(parent:getX() + (mX - dragStart.x), parent:getY() + (mY - dragStart.y));
+		dragStart.x = mX;
+		dragStart.y = mY;
+	end
+	-- }}}
+	function self:onMouseUpOutside(mX, mY) -- {{{
+		local parent = self:getParent();
+		if not parent then return false end
+		if isDragging then
+			isDragging = false;
+			return true;
+		end
+		return parent:onMouseUpOutside(mX, mY);
+	end
+	-- }}}
+	function self:onMouseMoveOutside(mX, mY) -- {{{
+		local parent = self:getParent();
+		if not parent then return false end
+		mX = getMouseX();
+		mY = getMouseY();
+		if not isDragging then return parent:onMouseMoveOutside(mX, mY) end
+
+		parent:setPosition(parent:getX() + (mX - dragStart.x), parent:getY() + (mY - dragStart.y));
+		dragStart.x = mX;
+		dragStart.y = mY;
+	end
+	-- }}}
+
+	return self;
+end
--- pzuioverhaul/media/lua/client/uio/Panel.lua
+++ pzuioverhaul/media/lua/client/uio/Panel.lua
@@ -3,15 +3,15 @@ require 'uio/BaseElement'
 UIO.Panel = {};
 
 function UIO.Panel.new(x, y, w, h)
-    local self = UIO.BaseElement.new(x, y, w, h);
+	local self = UIO.BaseElement.new(x, y, w, h);
 
-    local bgColor = { r = 0.1, g = 0.1, b = 0.1, a = 0.5 };
-    local borderColor = { r = 0.4, g = 0.4, b = 0.4, a = 0.8 };
+	local bgColor = { r = 0.1, g = 0.1, b = 0.1, a = 0.5 };
+	local borderColor = { r = 0.4, g = 0.4, b = 0.4, a = 0.8 };
 
-    function self:prerender()
-        self:drawRectangle('fill', x, y, w, h, bgColor);
-        self:drawRectangle('line', x, y, w, h, borderColor);
-    end
+	function self:prerender()
+		self:drawRectangle('fill', 0, 0, w, h, bgColor);
+		self:drawRectangle('line', 0, 0, w, h, borderColor);
+	end
 
-    return self;
+	return self;
 end
