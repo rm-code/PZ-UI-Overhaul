f9513e6bec12292494f58933fb67946002f218d1
Benjamin 'blindCoder' Schieder <benjamin@anderdonau.de>
2015-06-30 15:13:44 +0200

    show a Button with callback

--- pzuioverhaul/media/lua/client/uio/pzuiodemo.lua
+++ pzuioverhaul/media/lua/client/uio/pzuiodemo.lua
@@ -2,6 +2,11 @@ require 'uio/BaseElement'
 require 'uio/Panel'
 require 'uio/Draggable'
 require 'uio/Titlebar'
+require 'uio/Button'
+
+function callback(par1, par2, par3)
+	print("callback called with: "..tostring(par1)..", "..tostring(par2)..", "..tostring(par3));
+end
 
 local function pzuiodemo(player, context, items) -- {{{
 	local panel = UIO.Panel.new(100, 100, 200, 150);
@@ -12,6 +17,11 @@ local function pzuiodemo(player, context, items) -- {{{
 	local titlebar = UIO.Titlebar.new(0, 0, 200, 16);
 	titlebar:setTitle("UI Demo");
 	panel:addChild(titlebar);
+
+	local button = UIO.Button.new(60, 36, 40, 20);
+	button:setText("OK");
+	button:setOnClick(callback, getSpecificPlayer(0):getForname(), getSpecificPlayer(0):getSurname(), "DIEDIEDIE");
+	panel:addChild(button);
 
 	panel:addToUIManager();
 end
